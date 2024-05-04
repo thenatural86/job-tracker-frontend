4fe21a3b4b9d3944bb95c81ab4024ace6c78797d
thenatural86 <philip.smalls86@gmail.com>
2024-05-04 17:00:22 -0300

    initial router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>,
+  },
+  {
+    path: '/about',
+    element: <h1>About</h1>,
+  },
+])
 
 const App = () => {
-  return <h1>Jobify</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App

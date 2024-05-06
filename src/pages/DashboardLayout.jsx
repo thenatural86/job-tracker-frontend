6e72177c22d28b5907031f4be7da386081ecddb3
thenatural86 <philip.smalls86@gmail.com>
2024-05-06 16:05:55 -0300

    dashboard setup

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
-  return <h1>DashboardLayout</h1>
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
 }
 
 export default DashboardLayout

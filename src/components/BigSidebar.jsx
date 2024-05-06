3ab26c7ca4de74288820039b0cec3f3e65de9caa
thenatural86 <philip.smalls86@gmail.com>
2024-05-06 16:23:09 -0300

    dashboard structure

---
+++ src/components/BigSidebar.jsx
@@ -0,0 +1,9 @@
+import Wrapper from '../assets/wrappers/BigSidebar'
+
+import React from 'react'
+
+const BigSidebar = () => {
+  return <Wrapper>BigSidebar</Wrapper>
+}
+
+export default BigSidebar

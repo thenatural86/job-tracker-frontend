5cfc9a6ea90e88c774ddad4f4493a79536b1eabb
thenatural86 <philip.smalls86@gmail.com>
2024-05-06 10:03:18 -0300

    styled button

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`
 
 const Landing = () => {
-  return <h1>Landing</h1>
+  return (
+    <div>
+      <h1>Landing</h1>
+      <StyledBtn>styled btn</StyledBtn>
+    </div>
+  )
 }
 
 export default Landing

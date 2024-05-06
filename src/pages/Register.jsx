f323e4c16a53d4a4febc0e9560925e4e3ec83466
thenatural86 <philip.smalls86@gmail.com>
2024-05-06 14:07:52 -0300

    adds card to register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
-import { Logo } from '../components'
+import { FormRow, Logo } from '../components'
 
 const Register = () => {
   return (
@@ -9,19 +9,16 @@ const Register = () => {
       <form className='form'>
         <Logo />
         <h4>Register</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            className='form-input'
-            defaultValue='joe'
-            required
-          />
-        </div>
+        <FormRow type='text' name='name' defaultValue='joe' />
+        <FormRow
+          type='text'
+          name='lastName'
+          labelText='last name'
+          defaultValue='bob'
+        />
+        <FormRow type='text' name='location' defaultValue='earth' />
+        <FormRow type='email' name='email' defaultValue='joe@joe.com' />
+        <FormRow type='password' name='password' defaultValue='joejoe' />
 
         <button type='submit' className='btn btn-block'>
           submit

17d7ce12344533c3c79686fee615526be22b99d3
thenatural86 <philip.smalls86@gmail.com>
2024-05-07 13:07:34 -0300

    big sidebar functionality

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,9 +1,29 @@
-import Wrapper from '../assets/wrappers/BigSidebar'
-
 import React from 'react'
 
+import Wrapper from '../assets/wrappers/BigSidebar'
+import { useDashBoardContext } from '../pages/DashboardLayout'
+import Logo from './Logo'
+import NavLinks from './NavLinks'
+
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>
+  const { showSidebar } = useDashBoardContext()
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks isBigSidebar />
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default BigSidebar
